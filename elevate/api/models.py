85e2977599fb168ae10fee430930234073a510de
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2023-06-20 16:07:59 +0300

    first commit

---
+++ elevate/api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=600)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
