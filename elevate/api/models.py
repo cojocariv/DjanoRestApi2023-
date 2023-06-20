352c777ded4f9432deecbe560cb2433e31f3cda5
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2023-06-20 22:30:15 +0300

    1

--- elevate/api/models.py
+++ elevate/api/models.py
@@ -5,3 +5,14 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=600)
     price = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Car(models.Model):
+    carName = models.CharField(max_length=200)
+    transmision=models.CharField(max_length=200)
+    def __str__(self):
+        return self.carName
--- elevate/elevate/settings.py
+++ elevate/elevate/settings.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
+
+    'rest_framework',
     
 ]
 
