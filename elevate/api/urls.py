ba9f6a025a69bac4f9bb80ec3494134f618aab2a
Vasilii Cojocari <47657115+cojocariv@users.noreply.github.com>
2023-06-21 16:47:49 +0300

    l

--- elevate/api/urls.py
+++ elevate/api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    
-
-]
+        path("products", views.product_list, name="products"),
+        path('product/<int:pk>', views.product, name="product")
+               ]
--- elevate/api/views.py
+++ elevate/api/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+
+@api_view(["GET", "POST"])
+def product_list(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        # return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def product(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+    except Product.DoesNotExist():
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
